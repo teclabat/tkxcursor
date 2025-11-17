dd89e34821e8bc11476cf24b33c13f4c0e2a8a12
thomas <thomas@localhost.localdomain>
2025-11-18 00:03:11 +0100

    fix test bugs

--- tests/all.tcl
+++ tests/all.tcl
@@ -9,7 +9,7 @@
 
 package require tcltest
 ::tcltest::loadTestedCommands
-package require performance
+package require xcursor
 
 set ::tcltest::testSingleFile false
 set ::tcltest::testsDirectory [file dir [info script]]
@@ -35,8 +35,7 @@ if {[llength $::tcltest::matchFiles] > 0} {
 
 set timeCmd {clock format [clock seconds]}
 puts stdout "Tests began at [eval $timeCmd]"
-
-puts stdout "performance [package provide performance]"
+puts stdout "xcursor [package provide xcursor]"
 
 # Source each of the specified tests
 foreach file [lsort [::tcltest::getMatchingFiles]] {
